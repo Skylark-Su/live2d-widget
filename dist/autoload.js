7c16949af5f50f438c27fdc1bcf456b491c20e2a
Skylark <59167266+Skylark-Su@users.noreply.github.com>
2025-05-14 18:04:35 +0900

    Update autoload.js

--- dist/autoload.js
+++ dist/autoload.js
@@ -1,6 +1,7 @@
 // live2d_path 参数建议使用绝对路径
 // const live2d_path = 'https://fastly.jsdelivr.net/npm/live2d-widgets@0/';
-const live2d_path = '/live2d-widget/dist/';
+// const live2d_path = '/live2d-widget/dist/';
+const live2d_path = 'https://fastly.jsdelivr.net/gh/Skylark-Su/live2d-widget@1.0.1/dist/';
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
